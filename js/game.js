d4f21dfa07667b7384d572295c2e61dda45a07e0
Irene <amoreperdesi@gmail.com>
2023-12-23 17:36:26 +0100

    Improved JS

--- js/game.js
+++ js/game.js
@@ -1,52 +1,91 @@
 class Game {
-    constructor(){
-        this.gameIntro = document.getElementById("game-intro");
-        this.gameScreen = document.getElementById("game-screen");
-        this.gameEndScreen = document.getElementById("game-end");
+  constructor() {
+    this.gameIntro = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.possibleChoices= ["scissors", "paper", "rock"]
+  }
+
+  startGame() {
+    this.gameIntro.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntro.style.zIndex = "1";
+    this.gameScreen.style.zIndex = "2";
+  }
+
+  playerChoice(choice) {
+    if (choice === "scissors") {
+        const choicesLeft = [document.getElementById("user-paper"), document.getElementById("user-rock")]
+        choicesLeft[0].style.opacity  = 0//note: turn 1 for the value to show
+        choicesLeft[1].style.opacity = 0//note: turn 1
+    }
+    else if (choice === "paper"){
+        const choicesLeft = [document.getElementById("user-scissors"), document.getElementById("user-rock")]
+        choicesLeft[0].style.opacity  = 0
+        choicesLeft[1].style.opacity = 0
+    }
+    else if (choice === "rock"){
+        const choicesLeft = [document.getElementById("user-scissors"), document.getElementById("user-paper")]
+        choicesLeft[0].style.opacity  = 0
+        choicesLeft[1].style.opacity = 0
     }
 
-    startGame(){
-        this.gameIntro.style.display = 'none'
-        this.gameScreen.style.display = 'block'
-        this.gameIntro.style.zIndex = '1'
-        this.gameScreen.style.zIndex = '2'
+    this.cpuRandom()
+  }
+
+  cpuRandom() {
+    setTimeout(() => {
+    const randomChoice = Math.floor(Math.random()*this.possibleChoices.length)
+    const cpuChoice = this.possibleChoices[randomChoice]
+    const cpuMoveImg = document.getElementById("cpu-move")
+
+    
+    if (cpuChoice === "scissors"){
+        cpuMoveImg.src = "./images/scissors.png"
+        cpuMoveImg.alt = "scissors"
     }
-
-    gamePlay(){
-
+    else if (cpuChoice === "paper"){
+        cpuMoveImg.src = "./images/paper.png"
+        cpuMoveImg.alt = "paper"
     }
-
-    endGame(outcome){
-        this.gameScreen.style.display = 'none'
-        this.gameEndScreen.style.display = 'block'
-        this.gameScreen.style.zIndex = '0'
-        this.gameEndScreen.style.zIndex = '1'
-
-        if(outcome === "win"){
-            const myOutcome = document.getElementById("my-outcome")
-            myOutcome.src = "./images/win.png"
-            myOutcome.alt = "winner"
-
-            const pikaWinner = document.getElementById("pika-endimage")
-            pikaWinner.src = "./images/pika-win.png"
-            pikaWinner.alt = "pika winner"
-        }
-
-        else if(outcome === "loose"){
-            const myOutcome = document.getElementById("my-outcome")
-            myOutcome.src = "./images/game-over.png"
-            myOutcome.alt = "game over"
-
-            const pikaLoser = document.getElementById("pika-endimage")
-            pikaLoser.src = "./images/pika-loose.png"
-            pikaLoser.alt = "pika loser"
-        }
-
-        else if(outcome === "tie"){
-            const myOutcome = document.getElementById("my-outcome")
-            myOutcome.src = "./images/tie.png"
-            myOutcome.alt = "tie"
-        }
+    else if (cpuChoice === "rock"){
+        cpuMoveImg.src = "./images/rock.png"
+        cpuMoveImg.alt = "rock"
     }
+    }, 2000);
+
+  }
+
+  gamePlay() {
+
+  }
+
+  endGame(outcome) {
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+    this.gameScreen.style.zIndex = "0";
+    this.gameEndScreen.style.zIndex = "1";
+
+    if (outcome === "win") {
+      const myOutcome = document.getElementById("my-outcome");
+      myOutcome.src = "./images/win.png";
+      myOutcome.alt = "winner";
+
+      const pikaWinner = document.getElementById("pika-endimage");
+      pikaWinner.src = "./images/pika-win.png";
+      pikaWinner.alt = "pika winner";
+    } else if (outcome === "loose") {
+      const myOutcome = document.getElementById("my-outcome");
+      myOutcome.src = "./images/game-over.png";
+      myOutcome.alt = "game over";
+
+      const pikaLoser = document.getElementById("pika-endimage");
+      pikaLoser.src = "./images/pika-loose.png";
+      pikaLoser.alt = "pika loser";
+    } else if (outcome === "tie") {
+      const myOutcome = document.getElementById("my-outcome");
+      myOutcome.src = "./images/tie.png";
+      myOutcome.alt = "tie";
+    }
+  }
 }
-
--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
 window.onload = function () {
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
-    const gameWin = document.getElementById("game-win") //to cancel
-    const gameOver = document.getElementById("game-over") //to cancel
-    const gameTie = document.getElementById("game-tie")//to cancel
-    let game
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  const gameWin = document.getElementById("game-win"); //to cancel
+  const gameOver = document.getElementById("game-over"); //to cancel
+  const gameTie = document.getElementById("game-tie"); //to cancel
+  const possibleChoices = [document.getElementById("user-scissors"), document.getElementById("user-paper"), document.getElementById("user-rock")]
+  let game;
+
+  startButton.addEventListener("click", function () {
+    game = new Game();
+    game.startGame();
+  });
+
+  possibleChoices[0].addEventListener("click", function (){
+    game.playerChoice("scissors")
+    //remove event listener
+  })
+
+  possibleChoices[1].addEventListener("click", function (){
+    game.playerChoice ("paper")
+    //remove event listener
+  })
+
+  possibleChoices[2].addEventListener("click", function(){
+    game.playerChoice("rock")
+    //remove event listener
+  })
+
   
-    startButton.addEventListener("click", function () {
-      game = new Game()
-      game.startGame()
-    });
 
-    
 
-    
-    gameWin.addEventListener("click", function () {
-      game.endGame("win")
-    })//to cancel
 
-    gameOver.addEventListener("click", function () {
-      game.endGame("loose")
-    })//to cancel
+  gameWin.addEventListener("click", function () {
+    game.endGame("win");
+  }); //to cancel
 
-    gameTie.addEventListener("click", function () {
-      game.endGame("tie")
-    })//to cancel
+  gameOver.addEventListener("click", function () {
+    game.endGame("loose");
+  }); //to cancel
 
-    restartButton.addEventListener('click',()=>{
-      location.reload()
-      console.log()
-    })
-}
-  
+  gameTie.addEventListener("click", function () {
+    game.endGame("tie");
+  }); //to cancel
+
+  restartButton.addEventListener("click", () => {
+    location.reload();
+    console.log();
+  });
+};

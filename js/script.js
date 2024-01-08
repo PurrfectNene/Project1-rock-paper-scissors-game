c9eddbcb7c9bda1fbe9ad3e231e15ec4008847a6
Irene <amoreperdesi@gmail.com>
2024-01-09 00:59:11 +0100

    removed add elements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-  const gameWin = document.getElementById("game-win"); //to cancel
-  const gameOver = document.getElementById("game-over"); //to cancel
-  const gameTie = document.getElementById("game-tie"); //to cancel
   const possibleChoices = [
     document.getElementById("user-scissors"),
     document.getElementById("user-paper"),
@@ -31,18 +28,6 @@ window.onload = function () {
   function playRock() {
     game.gamePlay("rock");
   }
-
-  gameWin.addEventListener("click", function () {
-    game.endGame("win");
-  }); //to cancel
-
-  gameOver.addEventListener("click", function () {
-    game.endGame("loose");
-  }); //to cancel
-
-  gameTie.addEventListener("click", function () {
-    game.endGame("tie");
-  }); //to cancel
 
   restartButton.addEventListener("click", () => {
     location.reload();
